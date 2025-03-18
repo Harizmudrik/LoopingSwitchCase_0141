30a2b2fac10091b1ab833d2c2c678f2f517add75
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-03-18 20:33:49 +0700

    Mengatur seed untuk angka acak berdasarkan waktu saat ini

---
+++ file.2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+int main () {
+    //Perulangan while 
+    int x;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
+
+    srand(time(0));
+    x = rand() % 10;
+    cout << "Perulangan While " << endl;
+    cout << "Nilai x awal = " << x << endl;
+
+   
+
+}
