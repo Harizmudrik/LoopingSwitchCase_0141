4dfa626ecefadb7d42f04992e9840edfec739ef1
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-03-18 20:11:09 +0700

    Penambahan pecahan turun

---
+++ file1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    //Pencacah Naik 
+    int i;
+    for (i=0; i < 5; i++){
+        cout << "Nilai i = "<< i << "Lari keliling Lapangan" << endl;
+    }
+    cout << "Nilai i terakhir : " << i << endl;
+    //Pencacah Turun 
+    for (i=5; i > 0; i--){
+        cout << "Nilai i = "<< i << "Lari keliling Lapangan" << endl;
+    }
+    cout << "Nilai i terakhir : " << i << endl;
+}
